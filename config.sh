f530f3ed5d334391235031cb22f85389fc1e1c1e
chverma <538853+chverma@users.noreply.github.com>
2021-04-20 19:07:55 +0200

    Add config script

---
+++ config.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if [ $# -lt 3 ]
+then
+	echo "usage: ./config.sh mydomain.com users myorganization_name"
+        exit
+fi
+DOMAIN=$(echo $1 | cut -d. -f1)
+DOMAIN_TLD=$(echo $1 | cut -d. -f2)
+SUBDOMAIN_USERS_ADDRESS=$2
+ORG_NAME=$3
+PASSWORD=$(openssl rand -base64 32)
+
+
+sed  -i -e "s/mydomain/$DOMAIN/g" -e "s/domtld/$DOMAIN_TLD/g" -e "s/subdomain/$SUBDOMAIN_USERS_ADDRESS/g" -e "s/MYORG/$ORG_NAME/g" -e "s/passToChange/$PASSWORD/g" docker-compose.yml
