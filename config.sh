574dfbb9c41bb7ba0b8588da3380ee839ce024a6
chverma <538853+chverma@users.noreply.github.com>
2021-04-20 19:34:02 +0200

    Preserve a copy of the original docker-compose file

--- config.sh
+++ config.sh
@@ -5,6 +5,8 @@ then
 	echo "usage: ./config.sh mydomain.com users myorganization_name"
         exit
 fi
+
+cp docker-compose.yml docker-compose.yml.bak
 DOMAIN=$(echo $1 | cut -d. -f1)
 DOMAIN_TLD=$(echo $1 | cut -d. -f2)
 SUBDOMAIN_USERS_ADDRESS=$2
@@ -12,4 +14,4 @@ ORG_NAME=$3
 # The sed here prevents a slash
 PASSWORD=$(openssl rand -base64 32 | sed "s/\//#/g")
 
-sed  -e "s/mydomain/$DOMAIN/g" -e "s/domtld/$DOMAIN_TLD/g" -e "s/subdomain/$SUBDOMAIN_USERS_ADDRESS/g" -e "s/MYORG/$ORG_NAME/g" -e "s/passToChange/$PASSWORD/g" docker-compose.yml
+sed  -i -e "s/mydomain/$DOMAIN/g" -e "s/domtld/$DOMAIN_TLD/g" -e "s/subdomain/$SUBDOMAIN_USERS_ADDRESS/g" -e "s/MYORG/$ORG_NAME/g" -e "s/passToChange/$PASSWORD/g" docker-compose.yml
